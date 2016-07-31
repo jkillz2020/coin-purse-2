39e1483395f25e2a4112c2ae1a1748bbd5e39eb1
jkillz2020 <kilgore.josh@gmail.com>
2016-07-31 18:25:17 -0500

    add all functionality except penny bug

--- main.js
+++ main.js
@@ -10,14 +10,26 @@
 
 */
 
-function coinCounter () {
-  // Initialize a JavaScript object to hold the coins
+function coinCounter (num) {
   var coinPurse = {};
-
-  coinPurse.quarters = 0;
-
-  return coinPurse;
+    if (num >= 0.25) {
+      coinPurse.quarters = Math.floor(num/0.25);
+      num = (num - (coinPurse.quarters*0.25));
+    }
+    if (num >= 0.10 || num <= 0.24) {
+      coinPurse.dimes = Math.floor(num/0.10);
+      num = (num - (coinPurse.dimes*0.10));
+    }
+    if (num >= 0.05 || num <=0.09) {
+      coinPurse.nickels = Math.floor(num/0.05);
+      num = (num - (coinPurse.nickels*0.05));
+    }
+    if (num > 0.00 || num <= 0.04) {
+      coinPurse.pennies = Math.floor(num/0.01);
+      num = (num - (coinPurse.pennies*0.01));
+    }
+  return coinPurse || (num);
 }
 
-var coins = coinCounter()
-console.log();
+var coins = coinCounter(1.66);
+console.log(coins);
